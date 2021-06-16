b171dfa001b56b60fb9d91bd92467277b3590406
imakb4r <akba4ransari@gmail.com>
2021-06-16 08:48:46 +0530

    First Commit

---
+++ lib/info_screen.dart
@@ -0,0 +1,191 @@
+import 'package:covid_19/constant.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'widgets/my_header.dart';
+
+class InfoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            MyHeader(
+              image: "assets/icons/coronadr.svg",
+              textTop: "Get to Know",
+              textBottom: "About COVID-19.",
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Symptoms",
+                    style: kTitleTextstyle,
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SymptomsCard(
+                        image: "assets/images/headache.png",
+                        title: "Headache",
+                        isActive: true,
+                      ),
+                      SymptomsCard(
+                        image: "assets/images/caugh.png",
+                        title: "Cough",
+                        isActive: true,
+                      ),
+                      SymptomsCard(
+                        image: "assets/images/fever.png",
+                        title: "Fever",
+                        isActive: true,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Text("Preventions", style: kTitleTextstyle),
+                  SizedBox(height: 20),
+                  PreventCard(
+                    text:"Since the start of the coronavirus outbreak some "
+                        "places have fully embraced wearing facemasks",
+                    image:"assets/images/wear_mask.png",
+                    title: "Wear a Face Mask",
+                  ),
+                  PreventCard(
+                    text:"Wash Hand carefully.",
+                    image:"assets/images/wash_hands.png",
+                    title: "Wash Hands.",
+                  ),
+                  SizedBox(height: 50),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PreventCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String text;
+  const PreventCard({
+    Key key,
+    this.title,
+    this.image,
+    this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: SizedBox(
+        height: 156,
+        child: Stack(
+          alignment: Alignment.centerLeft,
+          children: [
+            Container(
+              height: 136,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow:[
+                  BoxShadow(
+                    offset: Offset(0,8),
+                    blurRadius: 24,
+                  )
+                ]
+              ),
+            ),
+            Image.asset(image),
+            Positioned(
+              left: 130,
+              child:Container(
+              padding: EdgeInsets.symmetric(horizontal: 20,
+                  vertical: 15),
+              height: 136,
+              width: MediaQuery.of(context).size.width - 170,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [Text(
+                  title,
+                  style: kTitleTextstyle.copyWith(
+                    fontSize: 16,
+                  ),
+                ),
+                  Text(text,
+                    style: TextStyle(
+                      fontSize:12,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                      child: SvgPicture.asset(
+                          "assets/icons/forward.svg",
+                      ),
+                  )
+               ],
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SymptomsCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final bool isActive;
+  const SymptomsCard({
+    Key key,
+    this.image,
+    this.title,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
+        boxShadow: [
+          isActive ?
+          BoxShadow(
+            offset: Offset(0,10),
+            blurRadius: 20,
+            color: kActiveShadowColor,
+          )
+          : BoxShadow(
+              offset: Offset(0, 3),
+              blurRadius: 6,
+          )
+        ],
+      ),
+      child: Column(
+        children: [
+          Image.asset(image,height: 90),
+          Text(
+            title,
+            style:TextStyle(
+              fontWeight: FontWeight.bold,
+            ) ,
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/counter.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:covid_19/constant.dart';
+
+class Counter extends StatelessWidget {
+  final int number;
+  final Color color;
+  final String title;
+  const Counter({
+    Key key,
+    this.number,
+    this.color,
+    this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(6),
+          height: 25,
+          width: 25,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color.withOpacity(.26),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.transparent,
+                border: Border.all(
+                  color: kInfectedColor,
+                  width: 2,
+                )
+            ),
+          ),
+        ),
+        SizedBox(height: 20),
+        Text(
+          "$number", //Active Case API data should be Used Here.
+          style: TextStyle(
+            fontSize: 40,
+            color: color,
+          ),
+        ),
+        Text(
+          title,
+          style: kSubTextStyle,
+        )
+      ],
+    );
+  }
+}
