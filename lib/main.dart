5f199093ff75a9d88ba602ef03bc87b611fb5a04
imakb4r <akba4ransari@gmail.com>
2021-06-21 09:09:43 +0530

    Getting Data from API

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,34 @@
+import 'dart:convert';
+
 import 'package:covid_19/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'widgets/counter.dart';
 import 'widgets/my_header.dart';
-import 'package:geolocator/geolocator.dart';
+//import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart';
-
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Covid 19',
       theme: ThemeData(
-
         scaffoldBackgroundColor: kBackgroundColor,
         fontFamily: "Poppins",
         textTheme: TextTheme(
@@ -44,46 +53,45 @@ class HomeScreen extends StatelessWidget {
               textBottom: "is stay at Home.",
             ),
             Container(
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-              height: 60,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(25),
-                border: Border.all(
-                  color: Color(0xFFE5E5E5),
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(25),
+                  border: Border.all(
+                    color: Color(0xFFE5E5E5),
+                  ),
                 ),
-              ),
-              child: Row(
-                children: [
-                  SvgPicture.asset(
-                    "assets/icons/maps-and-flags.svg",
-                  ),
-                  SizedBox(width : 20),
-                  Expanded(
-                    child: DropdownButton(
-                      isExpanded: true,
-                      underline: SizedBox(),
-                      icon: SvgPicture.asset("assets/icons/dropdown.svg"),
-                      value: "India",
-                      items: [
-                        'India',
-                        'Bangladesh',
-                        'United States',
-                        'Pakistan'
-                      ].map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (value) {},
+                child: Row(
+                  children: [
+                    SvgPicture.asset(
+                      "assets/icons/maps-and-flags.svg",
                     ),
-                  ),
-                ],
-              )
-            ),
+                    SizedBox(width: 20),
+                    Expanded(
+                      child: DropdownButton(
+                        isExpanded: true,
+                        underline: SizedBox(),
+                        icon: SvgPicture.asset("assets/icons/dropdown.svg"),
+                        value: "India",
+                        items: [
+                          'India',
+                          'Bangladesh',
+                          'United States',
+                          'Pakistan'
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (value) {},
+                      ),
+                    ),
+                  ],
+                )),
             SizedBox(height: 20),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 20),
@@ -93,11 +101,11 @@ class HomeScreen extends StatelessWidget {
                     text: TextSpan(
                       children: [
                         TextSpan(
-                          text:"Cases Update\n",
+                          text: "Cases Update\n",
                           style: kTitleTextstyle,
                         ),
                         TextSpan(
-                          text:"Newest update April 7",
+                          text: "Newest update April 7",
                           style: TextStyle(
                             color: kTextLightColor,
                           ),
@@ -141,7 +149,7 @@ class HomeScreen extends StatelessWidget {
                   Counter(
                     color: kDeathColor,
                     title: "Deaths",
-                    number: 87,
+                    number: 58431,
                   ),
                   Counter(
                     color: kRecovercolor,
@@ -183,7 +191,7 @@ class HomeScreen extends StatelessWidget {
                   color: Colors.white70,
                   boxShadow: [
                     BoxShadow(
-                      offset: Offset(0,10),
+                      offset: Offset(0, 10),
                       blurRadius: 30,
                       color: kShadowColor,
                     ),
@@ -195,23 +203,6 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                children: [
-                  Center(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(onSurface: Colors.red),
-                      onPressed: () {
-                        getData();
-                      },
-                      child: Text('Hello Flutter'),
-                    ),
-                  )
-                ],
-              ),
-            ),
           ],
         ),
       ),
@@ -219,20 +210,26 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-void getData() async{
- Response response =  await get(Uri.parse('https://api.covid19api'
-     '.com/summary'));
- print(response.body);
+void getData() async {
+  Response response = await get(Uri.parse('https://api.covid19api'
+      '.com/summary'));
+  if (response.statusCode == 200) {
+    String data = response.body;
+    var newConfirmed = jsonDecode(data) ['Global'] ['NewConfirmed'];
+    print("New Confirmed Cases are $newConfirmed ");
+    var totalConfirmed = jsonDecode(data) ['Global'] ['TotalConfirmed'];
+    print("Total Confirmed Cases are $totalConfirmed ");
+    var newDeaths = jsonDecode(data) ['Global'] ['NewDeaths'];
+    print("New Deaths are $newDeaths ");
+    var totalDeaths = jsonDecode(data) ['Global'] ['TotalDeaths'];
+    print("Total Deaths are $totalDeaths ");
+    var newRecovered = jsonDecode(data) ['Global'] ['NewRecovered'];
+    print("New Recovered are $newRecovered ");
+    var totalRecovered = jsonDecode(data) ['Global'] ['TotalRecovered'];
+    print("New Recovered are $totalRecovered ");
+    var lastupdate = jsonDecode(data) ['Global'] ['Date'];
+    print("Last Updates at $lastupdate ");
+    } else {
+    print(response.statusCode);
+  }
 }
-
-
-
-// Future getData() async{
-//   http.Response response = await http.get(Uri.parse('https://api.covid19api.com/summary'));
-//   if (response.statusCode == 200) {
-//     String data = response.body;
-//     return jsonDecode(data);
-//   } else {
-//     print(response.statusCode);
-//   }
-//}
